fbb314bada54f60807dcaac74d31e8456e9b1989
Winston Chang <winston@stdout.org>
2017-07-28 19:09:48 -0500

    Add 06_tabsets app

--- run_tests.R
+++ run_tests.R
@@ -1,9 +1,10 @@
 library(testthat)
 library(shinytest)
 
-test_that("Application works", {
+test_that("Applications work", {
   # Use compareImages=FALSE because the expected image screenshots were created
   # on a Mac, and they will differ from screenshots taken on the CI platform,
   # which runs on Linux.
   expect_pass(testApp("01_hello", compareImages = FALSE))
+  expect_pass(testApp("06_tabsets", compareImages = FALSE))
 })
