8510e571255eb4c0c76addd6ab1cc6f0455d9e74
Winston Chang <winston@stdout.org>
2017-08-02 15:43:41 -0500

    Convert to single app

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Example using shinytest with Travis CI
 
 This repository is an example to show how to use [shinytest](https://github.com/rstudio/shinytest) to test with Travis CI. For more information, see https://rstudio.github.io/shinytest/ci.html.
 
-In this repository, the applications are stored in subdirectories of the top level.
+In this repository, there is a single application stored in the top level.
--- run_tests.R
+++ run_tests.R
@@ -1,10 +1,9 @@
 library(testthat)
 library(shinytest)
 
-test_that("Applications work", {
+test_that("Application works", {
   # Use compareImages=FALSE because the expected image screenshots were created
   # on a Mac, and they will differ from screenshots taken on the CI platform,
   # which runs on Linux.
-  expect_pass(testApp("01_hello", compareImages = FALSE))
-  expect_pass(testApp("06_tabsets", compareImages = FALSE))
+  expect_pass(testApp(".", compareImages = FALSE))
 })
